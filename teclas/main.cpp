0dfd041a1f759bd7ffd5e49a1e6ac7e406029956
carolinahenaofernandez <167971626+carolinahenaofernandez@users.noreply.github.com>
2025-01-20 18:25:09 +0100

    Update main.cpp

--- teclas/main.cpp
+++ teclas/main.cpp
@@ -1,46 +1,17 @@
 #include "framework/application.h"
 #include "framework/utils.h"
 
-int main(int argc, char** argv) {
-    // Create the application object
-    Application* app = new Application("Computer Graphics", 1280, 720);
+int main(int argc, char **argv)
+{
+    // Launch the app (app is a global variable)
+    Application* app = new Application( "Computer Graphics", 1280, 720);
     app->Init();
 
-    // SDL Event loop
-    SDL_Event event;
-    bool running = true;
+    std::cout << "Starting loop..." << std::endl;
+    launchLoop(app);
 
-    while (running) {
-        // Process SDL events
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT:
-                    running = false; // Exit the loop
-                    break;
-
-                case SDL_KEYDOWN:
-                    app->OnKeyPressed(event.key); // Call your OnKeyPressed handler
-                    break;
-
-                case SDL_MOUSEBUTTONDOWN:
-                    app->OnMouseButtonDown(event.button); // Call your OnMouseButtonDown handler
-                    break;
-
-                case SDL_MOUSEBUTTONUP:
-                    app->OnMouseButtonUp(event.button); // Call your OnMouseButtonUp handler
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
-        // Call the Render method to update the screen
-        app->Render();
-    }
-
-    // Clean up
     SDL_Window* window = app->window;
+
     delete app;
 
     if (window) {
