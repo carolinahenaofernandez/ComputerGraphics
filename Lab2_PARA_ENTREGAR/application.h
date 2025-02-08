e0328b84b32705c671316869bef94527755e732e
carolinahenaofernandez <167971626+carolinahenaofernandez@users.noreply.github.com>
2025-02-08 14:58:17 +0100

    Update application.h

--- Lab2_PARA_ENTREGAR/application.h
+++ Lab2_PARA_ENTREGAR/application.h
@@ -17,5 +17,6 @@ public:
     float current_property; // Valor actual de la propiedad modificada (near, far, FOV)
     char property_mode; // 'N' = near, 'F' = far, 'V' = FOV
     Vector2 last_mouse_position;
+    int current_scene;
 
 };
