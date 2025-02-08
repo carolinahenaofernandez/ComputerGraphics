7e5b1a8d05fe44a4b0b828ec4961aff478d9dae9
carolinahenaofernandez <167971626+carolinahenaofernandez@users.noreply.github.com>
2025-02-08 14:33:24 +0100

    Update application.h

--- Lab2_PARA_ENTREGAR/application.h
+++ Lab2_PARA_ENTREGAR/application.h
@@ -14,5 +14,8 @@ public:
     float time;
     std::vector<Entity*> entities;
     Camera* camera;
+    float current_property; // Valor actual de la propiedad modificada (near, far, FOV)
+    char property_mode; // 'N' = near, 'F' = far, 'V' = FOV
+    Vector2 last_mouse_position;
 
 };
