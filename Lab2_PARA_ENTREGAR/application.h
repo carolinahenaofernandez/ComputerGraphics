83f3cdb70f2b7365654098fcba1181314b76ccce
carolinahenaofernandez <167971626+carolinahenaofernandez@users.noreply.github.com>
2025-02-08 11:16:20 +0100

    Create application.h

---
+++ Lab2_PARA_ENTREGAR/application.h
@@ -0,0 +1,17 @@
+#include "camera.h"
+
+class Application
+{
+public:
+
+//Ho has de ficar sota de: // Window
+
+    //SDL_Window* window = nullptr;
+    //int window_width;
+    //int window_height;
+
+    float time;
+    std::vector<Entity*> entities;
+    Camera* camera;
+
+};
