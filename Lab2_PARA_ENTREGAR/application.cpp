4137dd633d257aa0f0f8ae0bec922ed980d0d4e8
carolinahenaofernandez <167971626+carolinahenaofernandez@users.noreply.github.com>
2025-02-08 12:46:03 +0100

    Update application.cpp

--- Lab2_PARA_ENTREGAR/application.cpp
+++ Lab2_PARA_ENTREGAR/application.cpp
@@ -5,8 +5,8 @@ void Application::Init(void)
 {
     std::cout << "Initiating app..." << std::endl;
     camera = new Camera();
-    camera->LookAt(Vector3(0,0,1), Vector3(0,0.25,0), Vector3(0,-1,0));
-    camera->SetPerspective(45.0f, (float)framebuffer.width / (float)framebuffer.height, 0.1f, 50.0f);
+    camera->LookAt(Vector3(0, 0, 1), Vector3(0, 0.25, 0), Vector3(0, -1, 0));
+    camera->SetPerspective(45.0f, (float)framebuffer.width / (float)framebuffer.height, 0.1f, 100.0f);
 
     Mesh* lee = new Mesh();
     if (!lee->LoadOBJ("meshes/lee.obj")) {
@@ -14,12 +14,31 @@ void Application::Init(void)
         return;
     }
 
-    Entity* leeEntity = new Entity();
-    leeEntity->mesh = lee;
+    Entity* entity1 = new Entity();
+    entity1->mesh = lee;
+    entity1->color = Color(0,255,0);
+    entity1->id = 1;
+    entity1->model.Translate(-1.5f, 0.0f, 0.0f);
+    
+    Entity* entity2 = new Entity();
+    entity2->mesh = lee;
+    entity2->color = Color(0,0,255);
+    entity2->id = 2;
+    entity2->model.Translate(1.5f, 0.0f, 0.0f);
+    
+    Entity* entity3 = new Entity();
+    entity3->mesh = lee;
+    entity3->color = Color(255,255,255);
+    entity3->id = 3;
+    entity3->model.Translate(0.0f, 0.0f, 0.0f);
+    
+    entity1->model = Matrix44();
+    entity2->model = Matrix44();
+    entity3->model = Matrix44();
 
-    leeEntity->model = Matrix44();
-
-    entities.push_back(leeEntity);
+    entities.push_back(entity1);
+    entities.push_back(entity2);
+    entities.push_back(entity3);
 
     std::cout << "Lee model loaded successfully!\n";
 }
@@ -32,9 +51,18 @@ void Application::Render(void)
     std::cout << "Rendering frame..." << std::endl; // Debug message
     for (Entity* entity : entities){
         if (entity){
-            entity->Render(&framebuffer, camera, Color(255,255,255)); // White wireframe
+            entity->Render(&framebuffer, camera, entity->color); // White wireframe
             std::cout << "Rendering entity..." << std::endl;
         }
     }
     framebuffer.Render();
+}
+
+// Called after render
+void Application::Update(float seconds_elapsed)
+{
+    for(Entity* entity : entities){
+        if(entity)
+            entity->Update(seconds_elapsed);
+    }
 }
