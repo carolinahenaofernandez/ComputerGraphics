c75948ef24bfabe112eb12549bad7df47aac67da
carolinahenaofernandez <167971626+carolinahenaofernandez@users.noreply.github.com>
2025-02-08 11:19:59 +0100

    Create image.h

---
+++ Lab2_PARA_ENTREGAR/image.h
@@ -0,0 +1,19 @@
+class Image{
+   // A general struct to store all the information about a TGA file
+    //typedef struct sTGAInfo {
+        //unsigned int width;
+        //unsigned int height;
+        //unsigned int bpp; // Bits per pixel
+        //unsigned char* data; // Bytes with the pixel information
+    //} TGAInfo;
+
+public:
+    //unsigned int width;
+    //unsigned int height;
+    //unsigned int bytes_per_pixel = 3; // Bits per pixel
+
+
+//!!!!!Sol has de ficar la sequent linea:
+    //From Lab1:
+    void DrawLineDDA(int x0, int y0, int x1, int y1, const Color& color);
+}
