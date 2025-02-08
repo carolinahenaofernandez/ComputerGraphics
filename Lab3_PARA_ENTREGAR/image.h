002fdcd003abcc40a18cefa9b5b8779670dff1ee
carolinahenaofernandez <167971626+carolinahenaofernandez@users.noreply.github.com>
2025-02-08 18:59:50 +0100

    Update image.h

--- Lab3_PARA_ENTREGAR/image.h
+++ Lab3_PARA_ENTREGAR/image.h
@@ -6,4 +6,6 @@ public:
 //Añade esto pookie
     void ScanLineDDA(int x0, int y0, int x1, int y1, std::vector<std::pair<int, int>>& table, int minY);
     void DrawTriangle(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Color& borderColor, bool isFilled, const Color& fillColor);
+    void DrawTriangleInterpolated(const Vector3& p0, const Vector3& p1, const Vector3& p2, const Color& c0, const Color& c1, const Color& c2);
+
 }
